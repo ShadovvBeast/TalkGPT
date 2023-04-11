6bf6156919a692f80173e5e054bb7766a83d66db
Hagar Fisher <hagarfisher@gmail.com>
2023-04-11 13:55:32 +0300

    tidying up

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-import { ChatGPTUnofficialProxyAPI } from 'chatgpt'
-import express from 'express';
-import cors from 'cors';
-import * as openai from './openai.js';
+import express from "express";
+import cors from "cors";
+import { AxiosError } from "axios";
+import { initializeApi } from "./util.js";
 
-const app = express()
+const app = express();
 app.use(cors());
 const port = process.env.PORT || 3001;
 
-app.get('/', (req, res) => {
-    res.send('OK')
+app.get("/", (req, res) => {
+  res.send("OK");
 });
 
 let api: any;
@@ -23,12 +23,15 @@ if (typeof api.initSession === 'function')
 app.get('/gpt/:message', async (req, res) => {
     try {
         const response = await api.sendMessage(req.params.message);
-        res.json({answer: response});
-    } catch (e: any) {
-        res.json({error: e.toString()});
+    res.json({ answer: response });
+  } catch (error) {
+    const axiosError = error as AxiosError;
+    res
+      .status(axiosError.status ?? 500)
+      .json({ error: axiosError.response?.data });
     }
 });
 
 app.listen(port, () => {
-    console.log(`TalkGPT listening on port ${port}`)
+  console.log(`TalkGPT listening on port ${port}`);
 });
